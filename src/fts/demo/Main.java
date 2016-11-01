85856fb731561d64c8df9abefcdd077af966e7c1
Franco Catrin <fcatrin@gmail.com>
2016-11-01 14:58:50 -0300

    Fix property names. Dump result

--- src/fts/demo/Main.java
+++ src/fts/demo/Main.java
@@ -3,13 +3,17 @@ package fts.demo;
 import fts.core.Application;
 import fts.core.Window;
 import fts.swt.SWTFactory;
+import fts.views.View;
 
 public class Main {
 	public static void main(String[] args) {
 		Application app = new Application(new SWTFactory());
 		Window window = app.createWindow();
 		window.setTitle("First FTS window");
-		window.setContentView(app.inflateView("main"));
+		
+		View rootView = app.inflateView("main");
+		System.out.println(rootView);
+		window.setContentView(rootView);
 		window.open();
 		window.mainLoop();
 	}
